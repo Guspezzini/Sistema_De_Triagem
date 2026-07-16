b21068699b5a7ddfce8da604815af46ee2c606a7
Gustavo <gustavo_pezzini@estudante.sesisenai.org.br>
2026-07-16 11:11:19 -0300

    terceira versão

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
   output: {
     // Arquivo de saÃ­da final que o navegador vai ler
     filename: 'bundle.js', 
-    // O Webpack vai salvar dentro de uma pasta chamada "dist" na raiz do projeto
-    path: path.resolve(__dirname, 'dist'), 
+    // O Webpack vai salvar dentro de uma pasta chamada "public" na raiz do projeto
+    path: path.resolve(__dirname, 'public'), 
   },
   // MantÃ©m o cÃ³digo legÃ­vel no bundle.js facilitando a depuraÃ§Ã£o
   mode: 'development' 
